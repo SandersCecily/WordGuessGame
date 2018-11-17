0cebbb286fe0391dd3af617fe5a4e8f104d5b746
Cecily <Sanders.Cecily@gmail.com>
2018-11-17 18:26:01 -0500

    fixed earlier javascript issues

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,20 +1,48 @@
+$(document).ready(function(){
 //create a word bank
-let wordbank = ["roast", "bean", "espresso", "latte", "au lait", "cappucino", "americano", "affogato" , "cold brew", "cortado", "irish coffee", "direct trade", "single origin", "macchiato", "green bean", "ristretto"];
+    let wordbank = ["roast", "bean", "espresso", "latte", "au lait", "cappucino", "americano", "affogato" , "cold brew", "cortado", "irish coffee", "direct trade", "single origin", "macchiato", "green bean", "ristretto"];
 
-//pick a word from the word bank -- works
-function pickword(arr){
-    let selected = Math.floor(Math.random()*arr.length);
-    return selected;
-};
-
-//display the word -- broken, makes the word, cant display.
-function displayword (arr, i){
     
-    console.log(arr[i]);
-};
+    //pick a word from the word bank
+    function pickword(arr){
+        let selected = Math.floor(Math.random()*arr.length);
+        return selected;
+    };
 
-let selected = pickword(wordbank);
-parseInt(selected);
-displayword(wordbank, selected);
-//broken
-$(".target").append("<p>"+ wordbank[selected] +"</p>");
+
+    //start/reset game
+    $("button").click(function(){
+        //making variables
+        let selected = pickword(wordbank);
+        let word = wordbank[parseInt(selected)];
+        let lives = 10;
+        let won = 0;
+        let lost = 0;
+        let wrongguess = [];
+
+        //testin purpose
+        console.log(word);
+        newGame(word);
+
+        //display variables
+
+    });//end button click
+
+
+    //convert word to underscores -- needed many times
+    function newGame(word){
+        let progressAnswer = [];
+        for (let i =0; i < word.length; i++){
+            if(word[i] === " "){
+                progressAnswer[i] = " ";
+            }
+            else{
+                progressAnswer[i] = "_";
+            }
+        }
+        $(".targetme").html("<p>"+progressAnswer.join("")+"</p>");
+    };//end newgame function
+
+    // //displays word.
+    // $(".targetme").html("<p>"+word+"</p>");
+});
